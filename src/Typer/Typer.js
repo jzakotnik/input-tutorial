5f18262a7ca78892c261e63a3f25197d1e09975d
jzakotnik <jure@zakotnik.de>
2020-03-09 22:42:54 +0100

    updated typing exercise

--- src/Typer/Typer.js
+++ src/Typer/Typer.js
@@ -3,6 +3,8 @@ import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
+import Badge from "@material-ui/core/Badge";
+import MailIcon from "@material-ui/icons/Mail";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
 import Link from "@material-ui/core/Link";
@@ -12,6 +14,8 @@ import AndroidIcon from "@material-ui/icons/Android";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
+
+var levenshtein = require("js-levenshtein");
 
 const useStyles = makeStyles(theme => ({
   paper: {
@@ -33,21 +37,38 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-const match = inputText => {
-  return "Todo, some matching... " + inputText;
-};
+//TODO load this from some config file later
+const testSentences = [
+  "hallo welt",
+  "Hallo Welt",
+  "Eddy ist super!",
+  "Und noch etwas mit vielen Leerzeichen!!",
+  "Findest Du auch ein Fragezeichen?",
+  "Das hier sieht aus wie ein Gartenzaun: #"
+];
 
 export default function Typer() {
   const [textValue, setTextValue] = useState("");
+  const [level, setLevel] = useState(0);
+  const [errors, setErrors] = useState(9); //TODO fix this to reflect real value at startup
+  const [nextLevelDisabled, setNextLevelDisabled] = useState(true);
+
   const classes = useStyles();
+  const match = inputText => {
+    setTextValue(inputText);
+    const textSimilarity = levenshtein(inputText, testSentences[level]);
+    setErrors(textSimilarity);
+    setNextLevelDisabled(textSimilarity > 0);
+    return "Todo, some matching... " + inputText;
+  };
 
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
+        <Badge badgeContent={errors} color={errors > 0 ? "error" : "primary"}>
           <AndroidIcon />
-        </Avatar>
+        </Badge>
         <Typography component="h1" variant="h5">
           Tippe mir nach...
         </Typography>
@@ -59,30 +80,31 @@ export default function Typer() {
             fullWidth
             id="myInput"
             label="Schreibe hier"
+            autoComplete="off"
             name="myInput"
             value={textValue}
-            onChange={event => setTextValue(event.target.value)}
+            onChange={event => match(event.target.value)}
             autoFocus
           />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="template"
-            label="Schreibe mir nach"
-            value={match(textValue)}
-            id="template"
-          />
+          <Typography variant="h6" component="h2" gutterBottom>
+            Tippe diesen Satz:
+            <br /> <b>{testSentences[level]}</b>
+          </Typography>
 
           <Button
-            type="submit"
             fullWidth
             variant="contained"
             color="primary"
+            disabled={nextLevelDisabled}
             className={classes.submit}
+            onClick={() => {
+              setLevel((level + 1) % testSentences.length);
+              setTextValue("");
+              match("");
+              console.log("Entering next leveL: " + level);
+            }}
           >
-            Weiter gehts..
+            Super, nächstes Level..
           </Button>
         </form>
       </div>

c4e5581a54d34de3597e168369ab07e0854263ee
jzakotnik <jure@zakotnik.de>
2020-03-10 15:57:26 +0100

    updated story and layout

--- src/Typer/Typer.js
+++ src/Typer/Typer.js
@@ -1,14 +1,9 @@
-import React, { useState, useEffect } from "react";
-import Avatar from "@material-ui/core/Avatar";
+import React, { useState } from "react";
+
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
 import Badge from "@material-ui/core/Badge";
-import MailIcon from "@material-ui/icons/Mail";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
 
 import AndroidIcon from "@material-ui/icons/Android";
 import Typography from "@material-ui/core/Typography";
@@ -38,13 +33,22 @@ const useStyles = makeStyles(theme => ({
 }));
 
 //TODO load this from some config file later
-const testSentences = [
-  "hallo welt",
-  "Hallo Welt",
-  "Eddy ist super!",
-  "Und noch etwas mit vielen Leerzeichen!!",
-  "Findest Du auch ein Fragezeichen?",
-  "Das hier sieht aus wie ein Gartenzaun: #"
+const situationSentences = [
+  "Willkommen bei Eddys Abenteuer! Eddy das Erdmännchen möchte nach Hause. Helfe ihm und tippe den Satz.",
+  "Weiter geht es über die Wiesen.. Aber wo ist sein Zuhause...",
+  "Achtung, ein Fuchs, was macht er jetzt?",
+  "Da oben sieht es aus wie ein Adler...",
+  "Das Haus ist nur noch 30 Meter weit",
+  "Das hier sieht aus wie Eddys Tür!"
+];
+
+const actionSentences = [
+  "ich laufe los",
+  "Immer der Sonne nach",
+  "Ein Fuchs, jetzt laufe ich schneller!",
+  "Ich verstecke mich hinter dem Gartenzaun ###",
+  "Eddy legt den Endspurt für die letzten 30 Meter ein",
+  "Tür auf und rein!"
 ];
 
 export default function Typer() {
@@ -56,29 +60,30 @@ export default function Typer() {
   const classes = useStyles();
   const match = inputText => {
     setTextValue(inputText);
-    const textSimilarity = levenshtein(inputText, testSentences[level]);
+    const textSimilarity = levenshtein(inputText, actionSentences[level]);
     setErrors(textSimilarity);
     setNextLevelDisabled(textSimilarity > 0);
-    return "Todo, some matching... " + inputText;
+    return "Some matching... " + inputText;
   };
 
   return (
-    <Container component="main" maxWidth="xs">
+    <Container component="main" maxWidth="sm">
       <CssBaseline />
       <div className={classes.paper}>
         <Badge badgeContent={errors} color={errors > 0 ? "error" : "primary"}>
           <AndroidIcon />
         </Badge>
-        <Typography component="h1" variant="h5">
-          Tippe mir nach...
-        </Typography>
+        <Typography component="h1" variant="h5"></Typography>
         <form className={classes.form} noValidate>
+          <Typography variant="h6" component="h2" gutterBottom>
+            {situationSentences[level]}
+            <br /> <b>{actionSentences[level]}</b>
+          </Typography>
           <TextField
             variant="outlined"
             margin="normal"
             required
             fullWidth
-            id="myInput"
             label="Schreibe hier"
             autoComplete="off"
             name="myInput"
@@ -86,10 +91,6 @@ export default function Typer() {
             onChange={event => match(event.target.value)}
             autoFocus
           />
-          <Typography variant="h6" component="h2" gutterBottom>
-            Tippe diesen Satz:
-            <br /> <b>{testSentences[level]}</b>
-          </Typography>
 
           <Button
             fullWidth
@@ -98,10 +99,10 @@ export default function Typer() {
             disabled={nextLevelDisabled}
             className={classes.submit}
             onClick={() => {
-              setLevel((level + 1) % testSentences.length);
+              setLevel((level + 1) % actionSentences.length);
               setTextValue("");
               match("");
-              console.log("Entering next leveL: " + level);
+              console.log("Entering next level: " + level);
             }}
           >
             Super, nächstes Level..
